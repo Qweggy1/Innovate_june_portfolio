e2710cfbbcaae043c1470ef6176966d5e71ab71f
Qweggy <qweggy24@gmail.com>
2022-06-27 17:07:31 +0100

    Python updated saved

--- python_notes/recap.py
+++ python_notes/recap.py
@@ -5,3 +5,16 @@ greeting = ("Hello World")
 print(greeting)
 
 print("This is a string for displaying charecters")
+
+#Booleon TRUE AND FALSE
+#Float Number with DECIMAL 1.4
+#Interger Whole Numbers 14
+
+print(1234+1) #this is an interger/whole number
+print(12.34) #this is a float
+print(True)
+print(False) #these are booleons True & False
+print(None) #none - blank/nul data
+
+
+
