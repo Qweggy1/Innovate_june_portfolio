de158b92877256df3c435c55fbaafa956c9a548b
Qweggy <qweggy24@gmail.com>
2022-06-28 11:36:23 +0100

    Python recap part 1

--- python_notes/recap.py
+++ python_notes/recap.py
@@ -1,20 +1,73 @@
-print("This is my file")
+# import random
+# print("This is my file")
 
-greeting = ("Hello World")
+# greeting = ("Hello World")
 
-print(greeting)
+# print(greeting)
 
-print("This is a string for displaying charecters")
+# print("This is a string for displaying charecters")
 
-#Booleon TRUE AND FALSE
-#Float Number with DECIMAL 1.4
-#Interger Whole Numbers 14
+# #Booleon TRUE AND FALSE
+# #Float Number with DECIMAL 1.4
+# #Interger Whole Numbers 14
 
-print(1234+1) #this is an interger/whole number
-print(12.34) #this is a float
-print(True)
-print(False) #these are booleons True & False
-print(None) #none - blank/nul data
+# print(1234+1) #this is an interger/whole number
+# print(12.34) #this is a float
+# print(True)
+# print(False) #these are booleons True & False
+# print(None) #none - blank/nul data
+
+# print((greeting[1]))
+
+# print(greeting[-1])
+
+# print(greeting.upper())
+
+# print(greeting.lower())
+
+# print("HeLlO".capitalize())
+
+# print("hello welcome to code nation".count("o"))
+
+# print("Welcome to code Tnation".find("T"))
+
+# print("The quick brown fox".replace("fox","Frog"))
+
+# print(("The quick brown fox").strip("fox"))
+
+# print(random.random()) #Random Float num between 0,1
+
+# print(random.uniform(1,10)) #Random Float num between 1,10
+
+# print(random.randint(1,10)) #Random Integer between input
+
+# my_name="Qweggy" # Variables
+# my_age=29
+# student=True
+
+# print(my_name,my_age,student)
+
+# print("Hello my name is", my_name)
+# print("I am", my_age)
+
+# print("Hello my name is {} and I am {} years old".format(my_name, my_age)) # Older format method
+
+# f string, new best method
+# print(f"Hello my name is {my_name} and i am {my_age}")
+
+# print(5433+42821)
+# print(5473982-321414) # Mathmatics
+# print(234*343)
+# print(5**4)
+# print(12%2)
+
+# balance=1000
+# print(balance)
+# amount=30
+                            #Basic Cash Machine
+# balance=amount+balance
+# balance +=amount
+# print(balance)
 
 
 
