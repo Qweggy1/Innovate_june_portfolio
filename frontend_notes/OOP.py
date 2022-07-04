4eaf18396ca99bf0613e27a9c897e9ac9c8efe8c
Qweggy <qweggy24@gmail.com>
2022-07-04 15:50:03 +0100

    Media Enquiries and activitys added

---
+++ frontend_notes/OOP.py
@@ -0,0 +1,9 @@
+# OOP Notes #
+
+# Scalability
+
+# oop means we are trying to model objects with the code
+# Everything in python is an object.
+
+# this empty codespace is an object as well. 
+
---
+++ frontend_notes/main.py
@@ -0,0 +1,3 @@
+from person import Person
+
+liam = Person("Liam", 31, "6'7''")
