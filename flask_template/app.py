6bc9548fe59e2cd9f6235968165d242b4650b172
Qweggy <qweggy24@gmail.com>
2022-07-06 15:39:25 +0100

    Assignment completed/Added using Flask

--- flask_template/app.py
+++ flask_template/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 from views import my_view
 
 app = Flask(__name__)
 app.register_blueprint(my_view)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
