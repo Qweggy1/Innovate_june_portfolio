e2587156ec27ce9813ebf09f1ac91b4e04e536de
Qweggy <qweggy24@gmail.com>
2022-07-07 17:32:57 +0100

    Added work with Javascript, Starter website for main Assignment added

---
+++ jsnotes/script.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+// thisIsCamelCase - example of camel case
+
+let myName = "Qweggy"; // can be changed let
+console.log(myName);
+
+const fullName = "Blake"; // can not change a const
+console.log(fullName);
+
+myName = "Frogo";
+console.log(myName);
+
+if (myName==="Qweggy") {
+    console.log("Hello Qweggy")
+} else if (myName==="Frogo") { // Java script If statement Syntax
+    console.log("Hello Frogo")
+} else {
+    console.log("Who are you?")
+}
+
+function sayHello() {
+    console.log("Hello")
+}
+
+sayHello()
+
---
+++ main_asign/view.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, Blueprint, redirect, url_for
+
+my_view = Blueprint('my_view', __name__)
+@my_view.route("/")
+def index():
+    return render_template("index.html")
+
+@my_view.route('/aboutme')
+def aboutme():
+    return render_template("aboutme.html")
+
+@my_view.route('/aboutmusic')
+def aboutmusic():
+    return render_template("aboutmusic.html")
+
+# @my_view.route('/page3')
+# def page3():
+#     return render_template("page3.html")
+
+# @my_view.route('/page4')
+# def page4():
+#     return render_template("page4.html")
+
+# @my_view.route('/page5')
+# def page5():
+#     return render_template("page5.html")
+
+
+@my_view.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+@my_view.route('/javascript')
+@my_view.route('/js')
+@my_view.route('/home')
+def index_redirect():
+    return redirect(url_for("my_view.index"))
